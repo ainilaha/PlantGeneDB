8f7f8a1d73a733607b3cabd53798e051bf65034c
victoria02020411 <sc23n2c@leeds.ac.uk>
2025-05-23 10:57:07 +0800

    Update index.php

--- index.php
+++ index.php
@@ -75,7 +75,7 @@
                 <li><a href="transcriptomics.html">Transcriptomics</a></li>
                 <li><a href="metabolomics.html">Metabolomics</a></li>
                 <li><a href="Proteomics.html">Proteomics</a></li>
-                <li><a href="Microbiomics.html">Microbiomics</a></li>
+                <li><a href="microbiomics.php">Microbiomics</a></li>
                 <li><a href="Epigenomic.html">Epigenomic</a></li>
               </ul>
             </li>
